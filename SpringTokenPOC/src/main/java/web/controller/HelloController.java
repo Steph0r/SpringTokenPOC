72f979a4439b63861172562aa78c9e4279db7fd1
Steph0r <Steph.Neumann2@gmail.net>
2015-06-07 21:20:40 +0200

    Added a generated token on the Welcome page for ease of testing
    The ModHeader addon for chrome works well for testing.

--- SpringTokenPOC/src/main/java/web/controller/HelloController.java
+++ SpringTokenPOC/src/main/java/web/controller/HelloController.java
@@ -1,9 +1,17 @@
 package web.controller;
 
+import java.util.ArrayList;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import config.TokenHandler;
+import config.UserService;
  
 @Controller
 public class HelloController {
@@ -11,24 +19,20 @@ public class HelloController {
 	@RequestMapping(value = { "/", "/welcome**" }, method = RequestMethod.GET)
 	public ModelAndView welcomePage() {
  
+		UserService us = new UserService();
+        us.addUser(new User("admin", "admin", new ArrayList<GrantedAuthority>() {{ new SimpleGrantedAuthority("ROLE_ADMIN"); }} ));
+		TokenHandler th = new TokenHandler("tooManySecrets", us);
+		User admin = us.loadUserByUsername("admin");
+		String token = th.createTokenForUser(admin);
+		
 		ModelAndView model = new ModelAndView();
 		model.addObject("title", "Spring Security Hello World");
-		model.addObject("message", "This is the welcome page!");
+		model.addObject("message", "Try to access /admin with and without this token in the X-AUTH-TOKEN header field<br><br>" + token);
 		model.setViewName("hello");
 		return model;
  
 	}
-	
-	public ModelAndView test() {
-		 
-		ModelAndView model = new ModelAndView();
-		model.addObject("title", "test");
-		model.addObject("message", "This is the welcome page!");
-		model.setViewName("hello");
-		return model;
- 
-	}
- 
+
 	@RequestMapping(value = "/admin**", method = RequestMethod.GET)
 	public ModelAndView adminPage() {
  
--- SpringTokenPOC/src/main/webapp/WEB-INF/pages/hello.jsp
+++ SpringTokenPOC/src/main/webapp/WEB-INF/pages/hello.jsp
@@ -2,7 +2,6 @@
 <html>
 <body>
 	<h1>${title}</h1>	
-	<h1>${message}</h1>	
-	<p> Go to admin page </p>
+	<p>${message}</p>	
 </body>
 </html>
